c5aa7210cb426a477f550febccc3cd2a6114d678
Jose Alejandro Montana <JoseMontanaC@unal.edu.co>
2019-12-12 15:57:31 -0300

    moving files

--- Versions/read_matrix.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pylab as plt
-a=np.genfromtxt("matrix.txt")
-plt.imshow(a,cmap="tab20")
-plt.colorbar()
-plt.clim(0,100)
-plt.show()
--- Versions/run.sh
+++
@@ -1,8 +0,0 @@
-nproc=2
-for ((i=0; i<$nproc;i++))
-do
-	echo $i
-	mpirun -np 2 ./a.out 10 $i 0 3 3 > matrix_$i.txt
-done
-python Make_imagen.py
-
